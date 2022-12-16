6bc6460625d94f49c396064d7bb4428444ecab33
Teddy Parker <57333590+Teddy-Parker1388@users.noreply.github.com>
2022-12-15 22:29:17 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "instance_type" {
+
+
+  description = "This is for defining instance type of server"
+  default = "t2.micro"
+
+
+
+}
+
+
+variable "ami" {
+
+  description = "The AMI of instance"
+
+
+
+
+}
+
+
+variable "tag" {
+
+
+description = "Tag of server"
+
+
+
+}
